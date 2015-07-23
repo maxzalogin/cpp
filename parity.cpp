ceacfe715e70f10aacf0f8807486f34f79a67ea1
Maxim Zalogin <max.zalogin@gmail.com>
2015-07-24 01:38:47 +0300

    The parity of the number
    C++

---
+++ parity.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a(0), b(0);
+	cout << "Введите число, чтобы проверить его четность: ";
+	cin >> a;
+	b = a % 2;
+	if (b == 0)
+	{
+		cout << "\nЧисло " << a << " чётное.\n";
+	}
+	else
+	{
+		cout << "\nЧисло " << a << " нечётное.\n";
+	}
+	_getch();
+	return 0;
+}
